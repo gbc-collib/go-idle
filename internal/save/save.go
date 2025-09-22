1ce619632b6b218797ef0b5054c71935da9eece5
Collib <collin.stasiak@rsmus.com>
2025-09-22 14:31:05 -0400

    Add load func

--- internal/save/save.go
+++ internal/save/save.go
@@ -1,13 +1,27 @@
 package game
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/gbc-collib/go-idle/internal/game"
 )
 
-func load_from_file() *game.GameState {
+const SAVE_FILE = "./save.json"
 
+func load_from_file() *game.GameState, error {
+	data, err := os.ReadFile(SAVE_FILE)
+	if err != nil {
+		return nil, err
+	}
+	var gameState game.GameState
+	err = json.Unmarshal(data, gameState)
+		if err != nil {
+		return nil, err
+	}
+	return gameState, nil
 }
 
-func (gs GameState) save_to_file() {
-	json
+func (gs GameState) save_to_file() error {
+
 }
